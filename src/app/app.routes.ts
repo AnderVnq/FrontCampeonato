8eae6f4495982692da5af9dbb7975abfbab5ea02
Ander19 <ndrsnvenega@gmail.com>
2024-05-03 08:46:34 -0500

    adding routing for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { CampeonatoComponent } from './pages/campeonato/campeonato.component';
 import { EquiposComponent } from './pages/equipos/equipos.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DashEquipoComponent } from './components/dash-equipo/dash-equipo.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -29,6 +32,21 @@ export const routes: Routes = [
     {
         path:'equipos/:nombre/:id',
         component:EquiposComponent
+    },
+    {
+            path:'dashboard',
+            component:DashboardComponent,
+            title:'Gestion Equipos',
+            children:[
+                {
+                path:'equipos',pathMatch:'full',
+                component:DashEquipoComponent 
+                }
+            ]
+    },
+    {
+        path:'**',
+        component:PageNotFoundComponent
     }
     
 
